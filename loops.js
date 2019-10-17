ff4fe9f9977bdbf8f8b76e03eb26e9d17a0d3a82
nsabir <awesomeartist15@gmail.com>
2019-10-17 04:33:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -41,8 +41,8 @@ function doWhileLoop(num) {
   num = num + 1;
   do {
    console.log("I run once regardless.");
-   } while (function incrementVariable() < num); {
+   } function incrementVariable() {
   i = i + 1;
   return i;
-   }
+}
 }
